d2a263b729b0d45563452a9345d7beaf5faccf57
Dari Kimanius <dkimanius@gmail.com>
2024-02-27 20:59:58 +0100

    Revert last commit

--- positron/sha3d/train_arguments.py
+++ positron/sha3d/train_arguments.py
@@ -108,7 +108,7 @@ def append_train_arguments(parser):
     parser.add_argument(
         '--z_contrastive_margin',
         help='Contrastive learning margin for Z',
-        type=range_limited_float_type(0), default=1.0
+        type=range_limited_float_type(0), default=0.1
     )
 
     parser.add_argument(
@@ -120,7 +120,7 @@ def append_train_arguments(parser):
     parser.add_argument(
         '--s_contrastive_margin',
         help='Contrastive learning margin for S',
-        type=range_limited_float_type(0), default=0.3
+        type=range_limited_float_type(0), default=0.2
     )
 
     parser.add_argument(
